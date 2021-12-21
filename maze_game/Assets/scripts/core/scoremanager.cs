ad31a99b6e1ac01216f27f11c6f4bf83f59d3676
unknown <s113809@ap.be>
2021-12-21 20:41:31 +0100

    point script

---
+++ maze_game/Assets/scripts/core/scoremanager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoremanager : MonoBehaviour
+{
+    public static scoremanager instance;
+    public int scoremax;
+
+    public Text scoretext;
+
+    int score = 0;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext.text = "points: " + score.ToString() + "/" + scoremax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addpoint() {
+        score += 1;
+        scoretext.text = "points:" + score.ToString() + "/" + scoremax; 
+    }
+}
